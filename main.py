62ceab63617148a63b51a7746c3b52efa5fe238e
second <thirdtreadmill@icloud.com>
2024-02-24 19:53:03 -0800

    added filters and default path to file browser.

--- main.py
+++ main.py
@@ -3,13 +3,12 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.image import Image
-from kivy.uix.filechooser import FileChooserIconView
+from kivy.uix.popup import Popup
+from kivy.uix.filechooser import FileChooserListView
 from kivy.graphics.texture import Texture
-from kivy.core.window import Window
-from sklearn.cluster import KMeans
 import cv2
 import numpy as np
-import os
+import platform
 
 class ColorDetectionApp(App):
     def build(self):
@@ -18,8 +17,9 @@ class ColorDetectionApp(App):
 
         layout = BoxLayout(orientation='vertical', padding=10)
 
-        file_chooser = FileChooserIconView(filters=['*.png', '*.jpg', '*.jpeg', '*.gif'])
-        file_chooser.bind(on_submit=self.on_file_selected)
+        file_button = Button(text='Select Image', size_hint=(None, None), size=(150, 50))
+        file_button.bind(on_press=self.open_file_chooser)
+        layout.add_widget(file_button)
 
         self.image_preview = Image(allow_stretch=True)
         layout.add_widget(self.image_preview)
@@ -33,31 +33,37 @@ class ColorDetectionApp(App):
         detect_button.bind(on_press=self.detect_color)
         layout.add_widget(detect_button)
 
-        layout.add_widget(file_chooser)
         return layout
 
-    def on_file_selected(self, instance, selection, touch):
-        self.selected_image = selection[0]
-        self.image_preview.source = self.selected_image
+    def open_file_chooser(self, instance):
+        def on_selection(instance, selection, touch):
+            if selection:
+                self.selected_image = selection[0]
+                self.image_preview.source = self.selected_image
+                popup.dismiss()
+
+        content = BoxLayout(orientation='vertical')
+        file_chooser = FileChooserListView(path='C:\\Users' if platform.system() == 'Windows' else '/Users/', filters=['*.png', '*.jpg', '*.jpeg', '*.gif'])
+        file_chooser.bind(on_submit=on_selection)
+        content.add_widget(file_chooser)
+        popup = Popup(title='Select Image', content=content, size_hint=(None, None), size=(400, 400))
+        popup.open()
 
     def detect_color(self, instance):
         if self.selected_image:
             image = cv2.imread(self.selected_image)
             image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-            height, width = image.shape[:2]
-            new_height = int(height * 200 / max(height, width))
-            new_width = int(width * 200 / max(height, width))
-            image = cv2.resize(image, (new_width, new_height))
-            image_flattened = image.reshape((-1, 3))
+            pixels = image.reshape((-1, 3))
+            pixels = np.float32(pixels)
 
-            kmeans = KMeans(n_clusters=1)
-            kmeans.fit(image_flattened)
-            dominant_color = kmeans.cluster_centers_[0]
+            criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+            flags = cv2.KMEANS_RANDOM_CENTERS
+            _, labels, centers = cv2.kmeans(pixels, 1, None, criteria, 10, flags)
 
-            dominant_color = np.uint8([[[dominant_color[0], dominant_color[1], dominant_color[2]]]])
+            centers = np.uint8(centers)
 
-            rgb_value = tuple(dominant_color[0][0][::-1])
+            rgb_value = tuple(centers[0])
 
             self.rgb_label.text = f'RGB: {rgb_value}'
             hex_value = '#{:02x}{:02x}{:02x}'.format(*rgb_value)
